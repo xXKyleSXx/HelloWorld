ccff57e336f4305476ddafd8f6bdac3c59b4d79e
Kyle <kyles148@nycstudents.net>
2026-09-18 15:15:44 -0400

    Asked name

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("What's your name??");
+        Scanner s = new Scanner(System.in);
+        String name = s.nextLine();
+        System.out.println("Hello, " + name + "!");
     }
 }
